969300fc3cadbdab08038e2d5f54d4c04cfe0a0a
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-09-05 14:23:20 -0500

    Begin login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vehicles_app/screens/login_screen.dart';
  
 void main() => runApp(MyApp());
  
@@ -6,17 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Vehicles App',
+      home: LoginScreen(),
     );
   }
 }
