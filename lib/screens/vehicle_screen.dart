4ae2af5b0de4315609f65478560a690f2e3daacd
Juan Zuluaga <jzuluaga-oc@shamrocktradingcorp.com>
2021-10-09 20:24:24 -0500

    Part 076

--- lib/screens/vehicle_screen.dart
+++ lib/screens/vehicle_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:adaptive_dialog/adaptive_dialog.dart';
@@ -47,11 +48,32 @@ class _VehicleScreenState extends State<VehicleScreen> {
   bool _lineShowError = false;
   TextEditingController _lineController = TextEditingController();
 
+  String _color = '';
+  String _colorError = '';
+  bool _colorShowError = false;
+  TextEditingController _colorController = TextEditingController();
+
+  String _model = '';
+  String _modelError = '';
+  bool _modelShowError = false;
+  TextEditingController _modelController = TextEditingController();
+
+  String _plaque = '';
+  String _plaqueError = '';
+  bool _plaqueShowError = false;
+  TextEditingController _plaqueController = TextEditingController();
+
+  String _remarks = '';
+  String _remarksError = '';
+  bool _remarksShowError = false;
+  TextEditingController _remarksController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
     _getVehiclesTypes();
     _getBrands();
+    _loadFieldValues();
   }
 
   @override
@@ -212,18 +234,52 @@ class _VehicleScreenState extends State<VehicleScreen> {
   }
 
   Widget _showModel() {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        keyboardType: TextInputType.number,
+        controller: _modelController,
+        decoration: InputDecoration(
+          hintText: 'Ingresa model...',
+          labelText: 'Modelo',
+          errorText: _modelShowError ? _modelError : null,
+          suffixIcon: Icon(Icons.event),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _model = value;
+        },
+      ),
+    );
   }
 
   Widget _showPlaque() {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        controller: _plaqueController,
+        decoration: InputDecoration(
+          hintText: 'Ingresa placa...',
+          labelText: 'Placa',
+          errorText: _plaqueShowError ? _plaqueError : null,
+          suffixIcon: Icon(Icons.directions_car),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _plaque = value;
+        },
+      ),
+    );
   }
 
   Widget _showLine() {
     return Container(
       padding: EdgeInsets.all(10),
       child: TextField(
-        autofocus: true,
         controller: _lineController,
         decoration: InputDecoration(
           hintText: 'Ingresa línea...',
@@ -242,15 +298,87 @@ class _VehicleScreenState extends State<VehicleScreen> {
   }
 
   Widget _showColor() {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        controller: _colorController,
+        decoration: InputDecoration(
+          hintText: 'Ingresa color...',
+          labelText: 'Color',
+          errorText: _colorShowError ? _colorError : null,
+          suffixIcon: Icon(Icons.palette),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _color = value;
+        },
+      ),
+    );
   }
 
   Widget _showRemarks() {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        controller: _remarksController,
+        decoration: InputDecoration(
+          hintText: 'Ingresa comentarios...',
+          labelText: 'Comentarios',
+          errorText: _remarksShowError ? _remarksError : null,
+          suffixIcon: Icon(Icons.notes),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _remarks = value;
+        },
+      ),
+    );
   }
 
   Widget _showButtons() {
-    return Container();
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Guardar'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    return Color(0xFF120E43);
+                  }
+                ),
+              ),
+              onPressed: () => _save(), 
+            ),
+          ),
+          widget.vehicle.id == 0 
+            ? Container() 
+            : SizedBox(width: 20,),
+          widget.vehicle.id == 0 
+            ? Container() 
+            : Expanded(
+                child: ElevatedButton(
+                  child: Text('Borrar'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        return Color(0xFFB4161B);
+                      }
+                    ),
+                  ),
+                  onPressed: () => _confirmDelete(), 
+              ),
+          ),
+        ],
+      ),
+    );
   }
 
   void _takePicture() async {
@@ -404,5 +532,280 @@ class _VehicleScreenState extends State<VehicleScreen> {
     setState(() {
       _brands = response.result;
     });
+  }
+
+  void _save() {
+    if (!_validateFields()) {
+      return;
+    }
+
+    widget.vehicle.id == 0 ? _addRecord() : _saveRecord();
+  }
+
+  void _confirmDelete() async {
+    var response =  await showAlertDialog(
+      context: context,
+      title: 'Confirmación', 
+      message: '¿Estas seguro de querer borrar el registro?',
+      actions: <AlertDialogAction>[
+          AlertDialogAction(key: 'no', label: 'No'),
+          AlertDialogAction(key: 'yes', label: 'Sí'),
+      ]
+    );    
+
+    if (response == 'yes') {
+      _deleteRecord();
+    }
+  }
+
+  void _deleteRecord() async {
+    setState(() {
+      _showLoader = true;
+    });
+
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {
+        _showLoader = false;
+      });
+      await showAlertDialog(
+        context: context,
+        title: 'Error', 
+        message: 'Verifica que estes conectado a internet.',
+        actions: <AlertDialogAction>[
+            AlertDialogAction(key: null, label: 'Aceptar'),
+        ]
+      );    
+      return;
+    }
+
+    Response response = await ApiHelper.delete(
+      '/api/Vehicles/', 
+      widget.vehicle.id.toString(), 
+      widget.token
+    );
+
+    setState(() {
+      _showLoader = false;
+    });
+
+    if (!response.isSuccess) {
+      await showAlertDialog(
+        context: context,
+        title: 'Error', 
+        message: response.message,
+        actions: <AlertDialogAction>[
+            AlertDialogAction(key: null, label: 'Aceptar'),
+        ]
+      );    
+      return;
+    }
+
+    Navigator.pop(context, 'yes');
+  }
+
+  bool _validateFields() {
+    bool isValid = true;
+
+    if (_vehicleTypeId == 0) {
+      isValid = false;
+      _vehicleTypeIdShowError = true;
+      _vehicleTypeIdError = 'Debes seleccionar un tipo de vehículo.';
+    } else {
+      _vehicleTypeIdShowError = false;
+    }
+
+    if (_brandId == 0) {
+      isValid = false;
+      _brandIdShowError = true;
+      _brandIdError = 'Debes seleccionar una marca.';
+    } else {
+      _brandIdShowError = false;
+    }
+
+    if (_line.isEmpty) {
+      isValid = false;
+      _lineShowError = true;
+      _lineError = 'Debes ingresar una línea.';
+    } else {
+      _lineShowError = false;
+    }
+
+    if (_color.isEmpty) {
+      isValid = false;
+      _colorShowError = true;
+      _colorError = 'Debes ingresar un color.';
+    } else {
+      _colorShowError = false;
+    }
+
+    if (_model.isEmpty) {
+      isValid = false;
+      _modelShowError = true;
+      _modelError = 'Debes ingresar un modelo.';
+    } else {
+      int model = int.parse(_model);
+      if (model < 1900 || model > 3000) {
+        isValid = false;
+        _modelShowError = true;
+        _modelError = 'El modelo debe ser un número entre 1900 y 3000.';
+      } else {
+        _modelShowError = false;
+      }
+    }
+
+    if (!RegExp('[a-zA-Z]{3}[0-9]{2}[a-zA-Z0-9]').hasMatch(_plaque)) {
+      isValid = false;
+      _plaqueShowError = true;
+      _plaqueError = 'El formato de la placa es incorrecto.';
+    } else {
+      _plaqueShowError = false;
+    }
+
+    setState(() { });
+    return isValid;
+  }
+
+  void _addRecord() async {
+    setState(() {
+      _showLoader = true;
+    });
+
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {
+        _showLoader = false;
+      });
+      await showAlertDialog(
+        context: context,
+        title: 'Error', 
+        message: 'Verifica que estes conectado a internet.',
+        actions: <AlertDialogAction>[
+            AlertDialogAction(key: null, label: 'Aceptar'),
+        ]
+      );    
+      return;
+    }
+
+    String base64Image = '';
+    if (_photoChanged) {
+      List<int> imageBytes = await _image.readAsBytes();
+      base64Image = base64Encode(imageBytes);
+    }
+
+    Map<String, dynamic> request = {
+      'vehicleTypeId': _vehicleTypeId,
+      'brandId': _brandId,
+      'model': _model,
+      'plaque': _plaque.toUpperCase(),
+      'line': _line,
+      'color': _color,
+      'userId': widget.user.id,
+      'remarks': _remarks,
+      'image': base64Image,
+    };
+
+    Response response = await ApiHelper.post(
+      '/api/Vehicles/', 
+      request, 
+      widget.token
+    );
+
+    setState(() {
+      _showLoader = false;
+    });
+
+    if (!response.isSuccess) {
+      await showAlertDialog(
+        context: context,
+        title: 'Error', 
+        message: response.message,
+        actions: <AlertDialogAction>[
+            AlertDialogAction(key: null, label: 'Aceptar'),
+        ]
+      );    
+      return;
+    }
+
+    Navigator.pop(context, 'yes');
+  }
+
+  void _saveRecord() async {
+    setState(() {
+      _showLoader = true;
+    });
+
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {
+        _showLoader = false;
+      });
+      await showAlertDialog(
+        context: context,
+        title: 'Error', 
+        message: 'Verifica que estes conectado a internet.',
+        actions: <AlertDialogAction>[
+            AlertDialogAction(key: null, label: 'Aceptar'),
+        ]
+      );    
+      return;
+    }
+
+    Map<String, dynamic> request = {
+      'id' : widget.vehicle.id,
+      'vehicleTypeId': _vehicleTypeId,
+      'brandId': _brandId,
+      'model': _model,
+      'plaque': _plaque.toUpperCase(),
+      'line': _line,
+      'color': _color,
+      'userId': widget.user.id,
+      'remarks': _remarks,
+    };
+
+    Response response = await ApiHelper.put(
+      '/api/Vehicles/', 
+      widget.vehicle.id.toString(),
+      request, 
+      widget.token
+    );
+
+    setState(() {
+      _showLoader = false;
+    });
+
+    if (!response.isSuccess) {
+      await showAlertDialog(
+        context: context,
+        title: 'Error', 
+        message: response.message,
+        actions: <AlertDialogAction>[
+            AlertDialogAction(key: null, label: 'Aceptar'),
+        ]
+      );    
+      return;
+    }
+
+    Navigator.pop(context, 'yes');
+  }
+
+  void _loadFieldValues() {
+    _vehicleTypeId = widget.vehicle.vehicleType.id;
+    _brandId = widget.vehicle.brand.id;
+
+    _model = widget.vehicle.model.toString();
+    _modelController.text = _model;   
+
+    _plaque = widget.vehicle.plaque;
+    _plaqueController.text = _plaque;   
+
+    _line = widget.vehicle.line;
+    _lineController.text = _line;   
+
+    _color = widget.vehicle.color;
+    _colorController.text = _color;   
+
+    _remarks = widget.vehicle.remarks == null ? '' : widget.vehicle.remarks!;
+    _remarksController.text = _remarks;   
   }
 }
